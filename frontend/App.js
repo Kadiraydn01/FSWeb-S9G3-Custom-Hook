fcf47580941493da6e04b191e30f45a923b87994
Kadiraydn01 <kadiraydn01@gmail.com>
2023-08-23 20:32:06 +0300

    App.js Düzenlendi

--- frontend/App.js
+++ frontend/App.js
@@ -1,25 +1,19 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
+import useLocal from "./hooks/localStorageKullan";
+import useGece from "./hooks/geceModuAc";
 
 const App = () => {
-  const [coinData, setCoinData] = useState([]);
-  const [geceModu, setGeceModu] = useState(false);
-
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
-  }, []);
+  const { info } = useLocal(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+  );
+  const { data, setData } = useGece();
   return (
-    <div className={geceModu ? "dark-mode App" : "App"}>
-      <Navbar geceModu={geceModu} setGeceModu={setGeceModu} />
-      <Charts coinData={coinData} />
+    <div className={data ? "dark-mode App" : "App"}>
+      <Navbar geceModu={data} setGeceModu={setData} />
+      <Charts coinData={info} />
     </div>
   );
 };
